e59769aaf10ee235e3a1206ccf0a3b7d74a78f4a
Chukwu Precious <cprecious28@gmail.com>
2022-10-13 22:23:19 -0700

    corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - Entry
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programmingis like building a multi lingual puzzle");
+	puts("\"Programming is like building a multi lingual puzzle");
 	return (0);
 }
