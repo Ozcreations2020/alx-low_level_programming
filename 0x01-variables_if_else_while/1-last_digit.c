a3588ddfbd67999811b531fc99c41ccfe7035ba6
Chukwu Precious <cprecious28@gmail.com>
2022-10-16 20:12:14 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,18 +4,18 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int n;
-
 	int m;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;	
 	m = n % 10;
+
 	if (m > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	if (m == 0)
