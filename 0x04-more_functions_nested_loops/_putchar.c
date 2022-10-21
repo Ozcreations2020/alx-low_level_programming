3f3e7cd577feef99bcff7659197c720a603c7dc6
Chukwu Precious <cprecious28@gmail.com>
2022-10-20 19:43:13 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unitd.h>
+#include "main.h"
+
+/**
+ * -pitchar - writes the character c to stdout
+ *  @c: The character to print
+ *  Return: On success 1 else -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
