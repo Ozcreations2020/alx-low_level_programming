541dbac42881319cd3b775d74028032bf27ca781
Chukwu Precious <cprecious28@gmail.com>
2022-10-25 04:25:33 -0700

    Task 0

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,8 +4,8 @@
  * _putchar - writes character c to stdout
  * @c: char to be printed
  * Return: On success 1
- * */
-int_putchar(char c)
+ */
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
